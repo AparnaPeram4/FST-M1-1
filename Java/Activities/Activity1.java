5bee92f9b100925afb4ba5c4430ff1098cc780ea
AparnaPeram4 <91046007+AparnaPeram4@users.noreply.github.com>
2021-09-20 11:06:32 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,20 @@
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car BMW = new Car();
+		BMW.make =2014;
+		BMW.color = "Black";
+		BMW.transmission = "Manual";
+		//using car class method
+		BMW.displayCharacterstics();//This displays the values of all the variables
+		BMW.accelerate();//This prints "Car is moving forward.
+		BMW.brake();//This prints "Car has stopped."
+		
+		
+		
+		
+	}
+
+}
